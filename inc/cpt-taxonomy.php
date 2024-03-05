080ef18ffd69c78d69a399e8de3d148fa23396d1
William Wu <williamwuu3@gmail.com>
2024-03-05 14:26:42 -0800

    made news file staff, updated cpt-tax

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -4,6 +4,54 @@
 //-----------------------------------------
 function sd_register_custom_post_types()
 {
+		// Register Staff
+	$labels = array(
+		'name'                  => _x( 'Staff', 'post type general name' ),
+		'singular_name'         => _x( 'Staff', 'post type singular name'),
+		'menu_name'             => _x( 'Staff', 'admin menu' ),
+		'name_admin_bar'        => _x( 'Staff', 'add new on admin bar' ),
+		'add_new'               => _x( 'Add New', 'Staff' ),
+		'add_new_item'          => __( 'Add New Staff' ),
+		'new_item'              => __( 'New Staff' ),
+		'edit_item'             => __( 'Edit Staff' ),
+		'view_item'             => __( 'View Staff' ),
+		'all_items'             => __( 'All Staff' ),
+		'search_items'          => __( 'Search Staff' ),
+		'parent_item_colon'     => __( 'Parent Staff:' ),
+		'not_found'             => __( 'No Staff found.' ),
+		'not_found_in_trash'    => __( 'No Staff found in Trash.' ),
+		'archives'              => __( 'Staff Archives'),
+		'insert_into_item'      => __( 'Insert into Staff'),
+		'uploaded_to_this_item' => __( 'Uploaded to this Staff'),
+		'filter_item_list'      => __( 'Filter Staff list'),
+		'items_list_navigation' => __( 'Staff list navigation'),
+		'items_list'            => __( 'Staff list'),
+		'featured_image'        => __( 'Staff featured image'),
+		'set_featured_image'    => __( 'Set Staff featured image'),
+		'remove_featured_image' => __( 'Remove Staff featured image'),
+		'use_featured_image'    => __( 'Use as featured image'),
+);
+
+$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'show_in_nav_menus'  => true,
+		'show_in_admin_bar'  => true,
+		'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'Staff' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 5,
+		'menu_icon'          => 'dashicons-archive',
+		'supports'           => array( 'title', 'thumbnail', 'editor' ),
+);
+
+register_post_type( 'fwd-staff', $args );
 
     // Register Student CPT
     $labels = array(
@@ -89,8 +137,6 @@ function sd_register_taxonomies()
     register_taxonomy('sd-student-type', array('sd-student'), $args);
 }
 add_action('init', 'sd_register_taxonomies');
-
-
 
 
 // fixes front end urls.. for when user changes themes
