dda0f84c184c2ca6f3a98319663bd9fc6d3d4531
William Wu <williamwuu3@gmail.com>
2024-03-05 23:06:34 -0800

    deleted some stuff from pages-staff

--- page-staff.php
+++ page-staff.php
@@ -18,7 +18,12 @@
 <main id="primary" class="site-main">
 
 <?php 
+while ( have_posts() ) :
+	the_post();
 
+	get_template_part( 'template-parts/content', 'page' );
 
-
+endwhile; // End of the loop.
 ?>
+
+</main>
