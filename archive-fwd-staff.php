02ae1e39bd8b1f410b2133d1b7784ba8f8b3dbcb
William Wu <williamwuu3@gmail.com>
2024-03-11 23:41:38 -0700

    names are displaying again, info is not

--- archive-fwd-staff.php
+++ archive-fwd-staff.php
@@ -13,15 +13,24 @@
 
 get_header();
 ?>
+
 <main id="primary" class="site-main">
 	<?php
-	get_template_part('template-parts/content', 'page');
+	while (have_posts()): the_post();
+		get_template_part('template-parts/content', 'page');
+	endwhile;
+	?>
+
+	<h1>Staff</h1>
+
+	<?php
 	$terms = get_terms(
 		array(
 			'taxonomy' => 'fwd-staff-category',
 			'hide_empty' => false,
 		)
 	);
+
 	if ($terms && !is_wp_error($terms)) {
 		foreach ($terms as $term) {
 			$args = array(
@@ -33,13 +42,14 @@ get_header();
 					array(
 						'taxonomy' => 'fwd-staff-category',
 						'field' => 'slug',
-						'terms' => $term
+						'terms' => $term->slug
 					),
 				),
 			);
 			$query = new WP_Query($args);
+
 			echo '<section class=""><h2>' . esc_html__($term->name, 'fwd') . '</h2>';
-			// creating our services
+
 			while ($query->have_posts()) {
 				$query->the_post();
 				?>
@@ -48,32 +58,35 @@ get_header();
 						<?php the_title(); ?>
 					</h2>
 					<?php
-					// ACF form validation
 					if (function_exists('get_field')) {
+						// Check if the 'staff' field exists and display its value if it does
 						if (get_field('staff')) {
-							echo the_field('staff');
-							echo '<br>';
+								the_field('staff'); // Output the value of the 'staff' field
+								echo '<br>'; // Add a line break for formatting
 						}
+						// Check if the 'courses' field exists and display its value if it does
 						if (get_field('courses')) {
-							echo the_field('courses');
-							echo '<br>';
+								the_field('courses'); // Output the value of the 'courses' field
+								echo '<br>'; // Add a line break for formatting
 						}
+						// Get the value of the 'url' field
 						$link = get_field('url');
+						// Check if the 'url' field has a value and create a link if it does
 						if ($link): ?>
-							<a class="button" href="<?php echo esc_url($link); ?>">Instructor Website</a>
+								<!-- Output a link with the value of the 'url' field -->
+								<a class="button" href="<?php echo esc_url($link); ?>">Instructor Website</a>
 						<?php endif;
-					}
+				}
+				
 					?>
 				</article>
 				<?php
 			}
 			wp_reset_postdata();
-			// Add your WP_Query() code here
-			// Use $term->slug in your tax_query
-			// Use $term->name to organize the posts
 		}
 	}
 	?>
 </main><!-- #primary -->
+
 <?php
 get_footer();
